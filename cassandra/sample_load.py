d33b30eaa043b73bb51b73b158a21fbf133af66b
Laurent Bernaille <laurent.bernaille@gmail.com>
2015-03-22 00:33:16 +0100

    Sample python script to load data inside cassandra

---
+++ cassandra/sample_load.py
@@ -0,0 +1,18 @@
+from cassandra.cluster import Cluster
+import uuid
+
+cluster = Cluster(['192.168.33.22'])
+session = cluster.connect()
+
+session.execute("CREATE KEYSPACE BOHistory WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 2 };")
+
+session.execute("CREATE TABLE bohistory.deals (dealid varchar primary key,client varchar,product varchar,quantity int,price double);")
+
+for i in range(1000):
+  session.execute(
+    """
+    INSERT INTO bohistory.deals (dealid,client,product,quantity,price)
+    VALUES (%(dealid)s,%(client)s,%(product)s,%(quantity)s,%(price)s)
+    """,
+    {'dealid': str(uuid.uuid1()),'client':'Ffrench', 'product': 'beer', 'quantity': 42, 'price':89.43}
+  )
