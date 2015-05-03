e42792e6e9777216763d8f9ea6b80172377c8245
Laurent Bernaille <laurent.bernaille@gmail.com>
2015-05-03 19:15:22 +0200

    [ACC-117] Add Rabbitmq configuration in dockerfile, set up rabbitmq configuration for ariane in entrypoint, add a quick README with sample docker commands

--- ariane.deploy/community-master/docker-entrypoint.sh
+++ ariane.deploy/community-master/docker-entrypoint.sh
@@ -22,23 +22,44 @@ if [ "$1" = 'startup.sh' ]; then
 			exit 1
 		fi
 
+		if [ -z "$ARIANE_RABBITMQ_HOST" ]; then
+			echo >&2 'error: ariane is not configured and ARIANE_RABBITMQ_HOST not set'
+			echo >&2 '  Did you forget to add -e ARIANE_RABBITMQ_HOST=... ?'
+			exit 1
+		fi
+
 		if [ -z "$ARIANE_DB_USER" ]; then
 		    ARIANE_DB_USER="ariane"
 		fi
 		if [ -z "$ARIANE_DB_PASSWORD" ]; then
 		    ARIANE_DB_PASSWORD="password"
 		fi
+		if [ -z "$ARIANE_RABBITMQ_USER" ]; then
+		    ARIANE_RABBITMQ_USER="ariane"
+		fi
+		if [ -z "$ARIANE_RABBITMQ_PASSWORD" ]; then
+		    ARIANE_RABBITMQ_PASSWORD="password"
+		fi
+		if [ -z "$ARIANE_RABBITMQ_VHOST" ]; then
+		    ARIANE_RABBITMQ_VHOST="/ariane"
+		fi
 
         echo "Setting database endpoints"
         sed -i  -e '/##hibernateConnectionURL/s%: ".*"%: "jdbc:mysql://'$ARIANE_DIRECTORY_HOST':3306/ariane_directory"%' \
-                -e '/##hibernateConnectionPassword/s%: .*"%: "'$ARIANE_DB_PASSWORD'"%' \
-                -e '/##hibernateConnectionUsername/s%: .*"%: "'$ARIANE_DB_USER'"%' \
+                -e '/##hibernateConnectionPassword/s%: ".*"%: "'$ARIANE_DB_PASSWORD'"%' \
+                -e '/##hibernateConnectionUsername/s%: ".*"%: "'$ARIANE_DB_USER'"%' \
                 $ARIANE_HOME/ariane/installer/resources/configvalues/components/cuDirectoryJPAProviderManagedService.json
 
         sed -i  -e '/##hibernateConnectionURL/s%: .*"%: "jdbc:mysql://'$ARIANE_IDM_HOST':3306/ariane_idm"%' \
-                -e '/##hibernateConnectionPassword/s%: .*"%: "'$ARIANE_DB_PASSWORD'"%' \
-                -e '/##hibernateConnectionUsername/s%: .*"%: "'$ARIANE_DB_USER'"%' \
+                -e '/##hibernateConnectionPassword/s%: ".*"%: "'$ARIANE_DB_PASSWORD'"%' \
+                -e '/##hibernateConnectionUsername/s%: ".*"%: "'$ARIANE_DB_USER'"%' \
                 $ARIANE_HOME/ariane/installer/resources/configvalues/components/cuIDMJPAProviderManagedService.json
+
+        sed -i  -e '/##MHOST_FQDN/s%: ".*"%: "'$ARIANE_RABBITMQ_HOST'"%' \
+                -e '/##MHOST_USER/s%: ".*"%: "'$ARIANE_RABBITMQ_USER'"%' \
+                -e '/##MHOST_PASSWD/s%: ".*"%: "'$ARIANE_RABBITMQ_PASSWORD'"%' \
+                -e '/##MHOST_VHOST/s%: ".*"%: "'$ARIANE_RABBITMQ_VHOST'"%' \
+                $ARIANE_HOME/ariane/installer/resources/configvalues/components/cuInjectorMessagingManagedService.json
 
         echo
         echo "Installing plugins"
@@ -59,4 +80,5 @@ if [ "$1" = 'startup.sh' ]; then
 	
 fi
 
+echo "Starting ariane"
 exec "$@"
