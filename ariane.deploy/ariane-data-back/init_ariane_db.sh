6ef3516075c550b6b2af735330a5901be1a40f0c
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-07-14 12:28:22 +0200

    up ariane docker tooling

--- ariane.deploy/ariane-data-back/init_ariane_db.sh
+++ ariane.deploy/ariane-data-back/init_ariane_db.sh
@@ -23,4 +23,9 @@ mysql -u ariane --password=password -h 127.0.0.1 -P 3316 -D ariane_idm < $ARIANE
 
 mysql -u ariane --password=password -h 127.0.0.1 -P 3316 -D ariane_idm < $ARIANE_PROJECT_PATH/ariane.community.plugin.docker/distrib/installer/resources/sqlscripts/plugins/docker/idm_plugin_docker_insert.sql 
 
-mysql -u ariane --password=password -h 127.0.0.1 -P 3326 -D ariane_directory < ./ariane_directory.sql
+if [ -f ./ariane_idm_ux.sql ]; then
+    mysql -u ariane --password=password -h 127.0.0.1 -P 3316 -D ariane_idm < ./ariane_idm_ux.sql
+fi
+if [ -f ./ariane_directory.sql ]; then
+    mysql -u ariane --password=password -h 127.0.0.1 -P 3326 -D ariane_directory < ./ariane_directory.sql
+fi
